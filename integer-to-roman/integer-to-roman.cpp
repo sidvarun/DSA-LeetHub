0c02ee35037e6d23c02a0add4cb8dbca28231aed
sidvarun <75657609+sidvarun@users.noreply.github.com>
2021-08-17 18:29:24 +0530

    Time: 4 ms (85.17%), Space: 6 MB (49.30%) - LeetHub

---
+++ integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string res = "";
+        int value[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string  str[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        for(int i = 0; i<13; i++){
+            while(num >= value[i]){
+                num = num - value[i];
+                res += str[i];
+            }
+        }
+        return res;
+    }
+};
