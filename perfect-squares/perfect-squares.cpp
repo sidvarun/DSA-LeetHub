4f4889c2bd743aa639872be35039d0917671fdc3
Varun Kumar <75657609+sidvarun@users.noreply.github.com>
2021-08-26 02:53:40 -0700

    Time: 176 ms (46.98%), Space: 9.1 MB (56.41%) - LeetHub

--- perfect-squares/perfect-squares.cpp
+++ perfect-squares/perfect-squares.cpp
@@ -1,12 +1,10 @@
 // class Solution {
 // public:
 //     int numSquares(int n) {
-//         int dp[n + 1];
-//         memset(dp, INT_MAX, sizeof(dp));
+//         vector<int> dp(n + 1, INT_MAX);
 //         dp[0] = 0;
 //         for(int i = 1; i<=n; i++){
-//             for(int j = 1; j<=i; j++){
-//                 if(j*j <= i)
+//             for(int j = 1; j*j<=i; j++){
 //                     dp[i] = min(dp[i], 1 + dp[i - j*j]);
 //             }
 //         }
