6957202b61dcb4e7c302347495bf2abba736daa9
Varun Kumar <75657609+sidvarun@users.noreply.github.com>
2021-09-03 06:04:38 -0700

    Time: 1276 ms (5.09%), Space: 17 MB (14.87%) - LeetHub

---
+++ jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX);
+        dp[n - 1] = 0;
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = i + 1; j < n; j++){
+                if(i + nums[i] >= j && dp[j] != INT_MAX)
+                    dp[i] = min(dp[i], 1 + dp[j]);
+            }
+        }
+        return dp[0];
+    }
+};
