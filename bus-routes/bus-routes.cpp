189cfa13b3119e4a5da25dcfcb49ef5a21ffaede
Varun Kumar <75657609+sidvarun@users.noreply.github.com>
2021-11-05 23:57:47 +0530

    Time: 556 ms (15.58%), Space: 74.9 MB (25.24%) - LeetHub

---
+++ bus-routes/bus-routes.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        if(source == target)
+            return 0;
+        vector<unordered_set<int>> buses;
+        for(int i = 0; i<routes.size(); i++){
+            unordered_set<int> s;
+            for(auto x : routes[i])
+                s.insert(x);
+            buses.push_back(s);
+        }
+        
+        queue<int> q;
+        unordered_set<int> visited;
+        for(int i = 0; i<buses.size(); i++){
+            unordered_set<int> s = buses[i];
+            if(s.find(source) != s.end()){
+                q.push(i);
+                visited.insert(i);
+            }
+        }
+        
+        if(q.empty())
+            return -1;
+        
+        int res = 1;
+        
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i<size; i++){
+                int curr = q.front();
+                q.pop();
+                if(buses[curr].find(target) != buses[curr].end())
+                    return res;
+                for(int i = 0; i<buses.size(); i++){
+                    
+                    if(visited.find(i) == visited.end()){
+                        unordered_set<int> c;
+                        // unordered_set<int> a = buses[curr];
+                        // unordered_set<int> b = buses[i];
+                        for (int element : buses[curr]) {
+                          if (buses[i].count(element) > 0) {
+                            c.insert(element);
+                          }
+                        }
+                        if(c.size() > 0){
+                            visited.insert(i);
+                            q.push(i);
+                        }
+                    }
+                }
+            }
+            res++;
+        }
+        return -1;
+    }
+};
